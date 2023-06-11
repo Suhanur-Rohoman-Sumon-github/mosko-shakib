f21ba65ec5d54174313b0de5e689faca235d7bd4
suhanur rohoman sumon <sumonsuhanurrohoman@gmail.com>
2023-06-11 14:39:10 +0600

    add to cart condition added

--- src/componnet/map-data/SingleMapData.jsx
+++ src/componnet/map-data/SingleMapData.jsx
@@ -3,40 +3,57 @@ import { Rating } from '@smastrom/react-rating'
 import '@smastrom/react-rating/style.css'
 import useContexts from '../../hook/useContexts';
 import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 const SingleMapData = ({ item }) => {
     const { name, description, image, rating, Price, seats } = item
-    const {user} = useContexts()
-    const handleAddtoCart = (item) =>{
+    const navigat = useNavigate()
+    const { user } = useContexts()
+    const handleAddtoCart = (item) => {
         const { name, image, Price, } = item
         const email = user?.email
-        const cartsInfo ={
+        const cartsInfo = {
             name,
             image,
             Price,
             email
         }
-        fetch('http://localhost:5000/carts',{
-            method:'POST',
-            headers:{
-                'content-type' : 'application/json'
+        if (!user) {
+            Swal.fire({
+                title: 'please login',
+                text: "You won't be able to add cart",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'login'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigat('/login')
+                }
+            })
+        }
+        fetch('http://localhost:5000/carts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
             },
-            body:JSON.stringify(cartsInfo)
+            body: JSON.stringify(cartsInfo)
         })
-        .then(res=>res.json())
-        .then(data=>{
-           
-            if(data.insertedId > 0){
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'Your work has been saved',
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-            }
-            console.log(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+
+                if (data.insertedId > 0) {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your work has been saved',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+                console.log(data)
+            })
     }
     return (
         <div className="card card-compact w-full shadow-white border border-white shadow-xl hover:shadow-green-500 bg-opacity-30 hover:bg-gradient-to-br from-pink-500 to-green-500 animate-gradient  transition duration-300">
@@ -52,7 +69,7 @@ const SingleMapData = ({ item }) => {
                 <p className='card-text-secondary'>price: {Price}$</p>
                 <p className='card-text-secondary'>avilable seats : {seats}</p>
                 <div className="card-actions justify-end">
-                    <button onClick={()=>handleAddtoCart(item)} className="btn btn-primary w-full">add to cart</button>
+                    <button onClick={() => handleAddtoCart(item)} className="btn btn-primary w-full">add to cart</button>
                 </div>
             </div>
         </div>
